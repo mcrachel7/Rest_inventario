97ed6e22af657285e4d7f726cc858a56febe563c
mcrachel17 <mcrachel79@unitec.edu>
2021-11-06 12:29:32 -0600

    commit inicial

---
+++ laboratorio/src/models/hardware.model.ts
@@ -0,0 +1,75 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Hardware extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  Id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Procesador: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  MemoriaCache: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  MemoriaRam: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Almacenamiento: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  TipoDeDisco: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Marca: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Modelo: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  FechaDeCompra: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Hardware>) {
+    super(data);
+  }
+}
+
+export interface HardwareRelations {
+  // describe navigational properties here
+}
+
+export type HardwareWithRelations = Hardware & HardwareRelations;
